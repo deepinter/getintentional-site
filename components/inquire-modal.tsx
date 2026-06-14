7cb47e2a86df1ebd205e7afb7f83188467c5059b
Mike Harrison <mike@Mikes-MacBook-Air-2.local>
2026-06-14 16:14:48 +0200

    Make URL field optional, remove type=url validation

--- components/inquire-modal.tsx
+++ components/inquire-modal.tsx
@@ -115,7 +115,7 @@ export default function InquireModal() {
             <FloatingField label="Your name"               name="name"     required />
             <FloatingField label="Email address"          name="email"    type="email" required />
             <FloatingField label="Organisation / business name" name="business" required />
-            <FloatingField label="Website URL"            name="url"      type="url"  required />
+            <FloatingField label="Website URL"            name="url" />
             <FloatingField label="The issue you're facing" name="issue"  rows={2} required />
 
             <div className="pt-2 md:pt-4 space-y-3 md:space-y-4">
